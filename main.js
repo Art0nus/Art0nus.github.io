1802c95e545553af5961198e9a03b1d3bdd1eb43
artonus <77129263+Art0nus@users.noreply.github.com>
2022-10-29 18:35:30 +0300

    Delete main.js

--- main.js
+++
@@ -1,2 +0,0 @@
-$(document).ready(()=>{$(()=>{$('[data-toggle="tooltip"]').tooltip();});width=$(document).width();if(width>768){num_nb=350;}else{num_nb=50;}
-particlesJS.load('particles-js','particlesjs.json');});
