e6c23e7626903cef41423608185013eb02f5351f
artonus <77129263+Art0nus@users.noreply.github.com>
2022-10-29 11:37:56 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,2 @@
+$(document).ready(()=>{$(()=>{$('[data-toggle="tooltip"]').tooltip();});width=$(document).width();if(width>768){num_nb=350;}else{num_nb=50;}
+particlesJS.load('particles-js','particlesjs.json');});
